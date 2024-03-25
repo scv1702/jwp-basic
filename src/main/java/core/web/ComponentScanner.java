07e663be599111ffdf56fc0c3d4598440dcc8d06
신찬규(Shin Changyu) <63400830+scv1702@users.noreply.github.com>
2024-03-25 13:03:49 +0900

    feat(mvc): 컴포넌트 및 컴포넌트 스캔 기능 구현

---
+++ src/main/java/core/web/ComponentScanner.java
@@ -0,0 +1,36 @@
+package core.web;
+
+import core.web.exception.ComponentScanException;
+import java.lang.reflect.Constructor;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ComponentScanner {
+
+    private static final Logger log = LoggerFactory.getLogger(ComponentScanner.class);
+    private static final Reflections reflections = new Reflections();
+
+    public static Map<String, Object> scan() {
+        Map<String, Object> components = new HashMap<>();
+        try {
+            Set<Class<?>> componentClasses = reflections.getTypesAnnotatedWith(Component.class);
+            for (Class<?> componentclass: componentClasses) {
+                if (componentclass.isAnnotation()) {
+                    continue;
+                }
+                Constructor<?> constructor = componentclass.getDeclaredConstructor();
+                constructor.setAccessible(true);
+                Object instance = constructor.newInstance();
+                components.put(componentclass.getName(), instance);
+            }
+        } catch (Exception e) {
+            throw new ComponentScanException(e.getMessage());
+        }
+        log.info("Scanned components: {}", components);
+        return components;
+    }
+}
