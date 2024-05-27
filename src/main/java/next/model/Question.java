f287587475704c13b778be045d2b9b7932678fd3
Changyu Shin <scv1702@naver.com>
2024-05-27 21:25:59 +0900

    feat(qna): 질문 도메인 모델 추가

---
+++ src/main/java/next/model/Question.java
@@ -0,0 +1,66 @@
+package next.model;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Question {
+
+    private Long questionId;
+    private String writer;
+    private String title;
+    private String contents;
+    private LocalDateTime createdDate;
+    private Integer countOfAnswer;
+
+    public Question() {
+    }
+
+    public Question(Long questionId, String writer, String title, String contents) {
+        this.questionId = questionId;
+        this.writer = writer;
+        this.title = title;
+        this.contents = contents;
+        this.createdDate = LocalDateTime.now();
+        this.countOfAnswer = 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        if (this == o) return true;
+        if (!(o instanceof Question)) return false;
+
+        Question question = (Question) o;
+
+        return Objects.equals(questionId, question.questionId ) &&
+                Objects.equals(writer, question.writer) &&
+                Objects.equals(title, question.title) &&
+                Objects.equals(contents, question.contents) &&
+                Objects.equals(createdDate, question.createdDate) &&
+                Objects.equals(countOfAnswer, question.countOfAnswer);
+    }
+
+    public Long getQuestionId() {
+        return questionId;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public Integer getCountOfAnswer() {
+        return countOfAnswer;
+    }
+}
