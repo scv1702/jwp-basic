d93abecefbe187ba360853782dcc7f197fe3012c
Changyu Shin <scv1702@naver.com>
2024-06-24 10:35:19 +0900

    feat(core): LocalDateTimeConverter가 ms의 9자리까지 처리하도록 수정

--- src/main/java/core/jdbc/converter/LocalDateTimeConverter.java
+++ src/main/java/core/jdbc/converter/LocalDateTimeConverter.java
@@ -4,23 +4,24 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 
 public class LocalDateTimeConverter implements PropertyConverter<LocalDateTime> {
 
     private static final DateTimeFormatter FORMATTER = new DateTimeFormatterBuilder()
         .appendPattern("yyyy-MM-dd HH:mm:ss")
         .optionalStart()
-        .appendFraction(ChronoField.MILLI_OF_SECOND, 1, 3, true)
+        .appendFraction(ChronoField.MILLI_OF_SECOND, 1, 9, true)
         .optionalEnd()
         .toFormatter();
 
     @Override
     public String toString(final LocalDateTime value) {
-        return value.format(FORMATTER);
+        return value.truncatedTo(ChronoUnit.SECONDS).format(FORMATTER);
     }
 
     @Override
     public LocalDateTime fromString(final String value) {
-        return LocalDateTime.parse(value, FORMATTER);
+        return LocalDateTime.parse(value, FORMATTER).truncatedTo(ChronoUnit.SECONDS);
     }
 }
